3e9aaf8c5008eebd7bb0e8024ca16e5b5108f210
owenoweny <na_chi@naver.com>
2024-03-30 14:19:33 +0900

    test: Cars - 우승자 선정 로직 테스트 구현

--- src/test/java/racinggame/domains/CarsTest.java
+++ src/test/java/racinggame/domains/CarsTest.java
@@ -1,5 +1,6 @@
 package racinggame.domains;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,11 +12,17 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @DisplayName("게임에 참가하는 자동차들 관련 테스트")
 public class CarsTest {
+
+    private Cars cars;
+
+    @BeforeEach
+    void setUp() {
+        cars = Cars.of(List.of("pobi", "jseo", "owen"));
+    }
+
     @Test
     @DisplayName("입력받은 차 이름을 바탕으로 객체를 초기화")
     void initialize() {
-        Cars cars = Cars.of(List.of("pobi", "jseo", "owen"));
-
         assertThat(cars.values())
                 .isEqualTo(List.of(
                         Car.of("pobi"),
@@ -27,5 +34,35 @@ public class CarsTest {
     @DisplayName("중복된 이름의 차를 가진 경우엔 예외를 발생")
     void filterDuplicated() {
         assertThatThrownBy(() -> Cars.of(List.of("jseo", "jseo"))).isInstanceOf(InputMismatchException.class);
+    }
+
+    @Test
+    @DisplayName("offset이 가장 큰 차가 우승자가 된다.")
+    void chooseWinner() {
+        List<Car> carList = cars.values();
+        Car pobi = carList.get(0);
+        Car jseo = carList.get(1);
+        pobi.move();
+        pobi.move();
+        jseo.move();
+
+        assertThat(cars.winners())
+                .hasSize(1)
+                .contains(pobi);
+    }
+
+    @Test
+    @DisplayName("offset이 가장 큰 차가 여러 대일 경우, 모두 우승자가 된다.")
+    void chooseWinners() {
+        List<Car> carList = cars.values();
+        Car pobi = carList.get(0);
+        Car jseo = carList.get(1);
+        pobi.move();
+        jseo.move();
+
+        assertThat(cars.winners())
+                .hasSize(2)
+                .contains(pobi)
+                .contains(jseo);
     }
 }
